02b9269af1b909a386da5ca6fa00075e9161e6bd
randunu94 <randunu.prabash@chakray.co.uk>
2025-06-02 19:17:00 +0100

    updated the reservation service

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../model/reservation';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
-  constructor() { }
+  private reservations : Reservation[] = []
+
+  getReservations() : Reservation[]{
+    return this.reservations
+  }
+
+  getReservation(id : String ) : Reservation | undefined{
+    return this.reservations.find(res => res.id === id)
+  }
+
+  addReservation(reservation : Reservation) : void{
+    this.reservations.push(reservation)
+  }
+
+  deleteReservation(id : String) : void{
+      let index = this.reservations.findIndex(res => res.id === id)
+      this.reservations.splice(index,1)
+  }
+
+  updateReservation(reservation : Reservation) : void{
+      let index = this.reservations.findIndex(res => res.id === reservation.id)
+      this.reservations[index] = reservation
+  }
+
 }
