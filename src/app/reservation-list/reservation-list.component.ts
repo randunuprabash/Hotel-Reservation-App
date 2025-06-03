339a9f5cfa7c9ec23446f2fe8d59368f6535df8a
randunu94 <randunu.prabash@chakray.co.uk>
2025-06-03 18:57:14 +0100

    Implemented reservations list

--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Reservation } from '../model/reservation';
+import { ReservationService } from '../reservation/reservation.service';
 
 @Component({
   selector: 'app-reservation-list',
   templateUrl: './reservation-list.component.html',
   styleUrls: ['./reservation-list.component.css']
 })
-export class ReservationListComponent {
+export class ReservationListComponent implements OnInit {
+
+  reservations : Reservation[] = []
+
+  constructor(private reservationService : ReservationService){}
+
+  ngOnInit(): void {
+    this.reservations = this.reservationService.getReservations()
+  }
 
 }
