67f1a85e99bdb3cbdc045e3bc88f2215c22e4ca2
randunu94 <randunu.prabash@chakray.co.uk>
2025-06-04 07:08:11 +0100

    Styling the home component

--- src/app/reservation/reservation.module.ts
+++ src/app/reservation/reservation.module.ts
@@ -4,7 +4,7 @@ import { ReservationFormComponent } from '../reservation-form/reservation-form.c
 import { ReservationListComponent } from '../reservation-list/reservation-list.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, ActivatedRoute } from '@angular/router';
-
+import { HomeModule } from '../home/home.module';
 
 
 @NgModule({
@@ -16,7 +16,8 @@ import { RouterModule, ActivatedRoute } from '@angular/router';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule
+    RouterModule,
+    HomeModule
   ]
 })
 export class ReservationModule { }
